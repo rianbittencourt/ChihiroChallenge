92b6dc022bcec475d9a68ad4642509de5182de9c
rianbittencourt <rian.bitten2@gmail.com>
2024-03-12 14:01:02 -0300

    fix: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,22 @@
-"use client";
 import ImageComponent from "./components/ImageComponent";
 import LeftSection from "./components/LeftSection";
 import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-
-  @media (max-width: 1000px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const BoxImage = styled.div`
-  order: 1;
-  width: 500px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 40px;
-  margin-top: 100px;
-
-  @media (max-width: 1000px) {
-    margin-right: 0px;
-    margin-top: 50px;
-    width: 400px;
-  }
-  @media (max-width: 768px) {
-    width: 300px;
-  }
-`;
+import Head from "next/head";
+import { Container, BoxImage } from "./styles";
+export const metadata: Metadata = {
+  title: "Viagem de Chihiro",
+  description: "Challenge",
+  icons: {
+    icon: "/favicon.ico?v=4",
+  },
+};
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Container>
         <LeftSection></LeftSection>
         <BoxImage>

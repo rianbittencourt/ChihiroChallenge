0f59786ad491774acd85c05a9c7e3f1a7a043c46
rianbittencourt <rian.bitten2@gmail.com>
2024-03-12 13:07:42 -0300

    feat: interface create

--- src/app/components/ImageComponent.tsx
+++ src/app/components/ImageComponent.tsx
@@ -1,22 +1,19 @@
-import styled, { keyframes } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 
-const floatingAnimation = keyframes`
-  0%, 100% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-20px);
-  }
-`;
+interface ImageComponentProps {
+  src: string;
+  alt: string;
+}
 
 const FloatingImage = styled.img`
   max-width: 100%;
   height: auto;
-  animation: ${floatingAnimation} 3s infinite ease-in-out;
+
 `;
 
-const ImageComponent = ({ src, alt, }) => {
-  return <FloatingImage src={src} alt={alt}  />;
+const ImageComponent: React.FC<ImageComponentProps> = ({ src, alt }) => {
+  return <FloatingImage src={src} alt={alt} />;
 };
 
 export default ImageComponent;

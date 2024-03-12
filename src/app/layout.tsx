186865a9b4d65afd49dc4c4d0645fc4c4a2b6d5c
rianbittencourt <rian.bitten2@gmail.com>
2024-03-12 08:43:09 -0300

    feat: first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from './components/header'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Header></Header>
       <body className={inter.className}>{children}</body>
     </html>
   )
